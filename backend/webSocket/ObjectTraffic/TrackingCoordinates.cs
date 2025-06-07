fcc2abdbc7e81bb87baa2bd2239b847dae342828
Antony Mncube <AntonyM@pepkorpl.co.za>
2025-06-07 17:13:08 +0200

    SignalR

---
+++ backend/webSocket/ObjectTraffic/TrackingCoordinates.cs
@@ -0,0 +1,8 @@
+﻿namespace ObjectTraffic
+{
+    public class TrackingCoordinates
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
---
+++ backend/webSocket/ObjectTraffic/TrackingHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ObjectTraffic
+{
+    public class TrackingHub : Hub
+    {
+        public async Task ReceiveCoordinates(string user, TrackingCoordinates trackingCoordinates)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, trackingCoordinates);
+        }
+
+    }
+}
