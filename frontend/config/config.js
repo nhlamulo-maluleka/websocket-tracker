473bfde209a1c97f1773e755c8429e8ddb451f75
nhlamulo maluleka <nhlamulo.silas@gmail.com>
2025-06-07 23:17:05 +0200

    Two way communication established

--- frontend/config/config.js
+++ frontend/config/config.js
@@ -1,4 +1,4 @@
 window.ENV = {
-    GOOGLE_API_KEY: "AIzaSyBOhq9DSdvzr__-PlFGhqKLpEGMFxQl_Fw",
+    GOOGLE_API_KEY: "",
     SOCKER_SERVER_URL: "https://sp6k4xqn-5034.inc1.devtunnels.ms/tracking"
 }
